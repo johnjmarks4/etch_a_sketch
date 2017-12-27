cba6ae6a94f386fe8213626505621752bb9f8bc7
John Marks <johnjmarks4@gmail.com>
2017-12-26 21:46:34 -0600

    Add rows

--- script.js
+++ script.js
@@ -1,27 +1,43 @@
 function createGrid() {
-  for (i=0; i < 256; i++) {
+  console.log("ran");
+  var rowDepth = parseInt(prompt("How many squares should the grid be on the top?"));
+  for (i=0; i < rowDepth; i++) {
     var content = document.querySelector('#container');
-    content.setAttribute("style", "display: flex; flex-flow: row; flex-wrap: wrap");
     const div = document.createElement("div");
-    div.setAttribute("class", "gridBox");
-    div.setAttribute("style", "width: 60px; height: 60px; margin-left: 20px; margin-top: 20px; order: " + i);
-    div.addEventListener("mouseover", function() {
-      div.style.backgroundColor = "green";
-    })
+    div.setAttribute("class", "gridRow");
+    div.setAttribute("style", "width: 100%; height: 100px; display: flex; flex-flow: row; flex-wrap: wrap");
     content.appendChild(div);
   }
 }
 
+function fillRow() {
+  var rowWidth = parseInt(prompt("How many squares should the grid be on the side?"));
+  rows = document.querySelectorAll(".gridRow");
+
+  rows.forEach(function(row) {
+    for (i=0; i < rowWidth; i++) {
+      const div = document.createElement("div");
+      div.setAttribute("class", "gridBox");
+      div.setAttribute("style", "width: 60px; height: 60px; margin-left: 20px; margin-top: 20px; order: " + i);
+      div.addEventListener("mouseover", function() {
+        div.style.backgroundColor = "green";
+      })
+      row.appendChild(div);
+    }
+  }
+)}
+
 createGrid();
+fillRow();
 
 var button = document.querySelector('#clear');
 button.setAttribute("style", "margin-right: 90%");
 button.addEventListener("click", function() {
   var content = document.querySelector('#container');
-  var grid = document.querySelectorAll('.gridBox');
-  console.log(grid);
-  grid.forEach((div) => {
-    content.removeChild(div);
+  var row = document.querySelectorAll('.gridRow');
+  row.forEach((row) => {
+    content.removeChild(row);
   })
   createGrid();
+  fillRow();
 })
